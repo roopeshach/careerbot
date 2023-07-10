a8b47ac69f719cefe3b479a5e47657b921ee9473
Roopesh Acharya <rupeshacharya2014@gmail.com>
2023-07-10 08:35:38 +0545

    db schema in models

---
+++ Main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class CareerAssessment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+
+class CareerSuggestion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    suggestion = models.CharField(max_length=255)
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class InterviewTip(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tip = models.TextField()
+
+class IndustryInsight(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    industry = models.CharField(max_length=255)
+    job_roles = models.TextField()
+    future_prospects = models.TextField()
+
+class UserFeedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feedback = models.TextField()
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255)
+
